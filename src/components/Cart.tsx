df23e6f66793c5d61411de263b229256bcae8850
Dian4ik <jakie16@yandex.ru>
2024-10-18 21:25:48 +0800

    add cart total price + button

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,5 +1,4 @@
-// import { useCartStore } from '@/app/zustand/use-cart'
-import { Sheet, SheetClose, SheetContent, SheetHeader, SheetTrigger } from './ui/sheet'
+import { Sheet, SheetClose, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from './ui/sheet'
 import { GrShop } from 'react-icons/gr'
 import Link from 'next/link'
 import { FaMinus, FaPlus, FaRegTrashAlt } from 'react-icons/fa'
@@ -19,7 +18,9 @@ const Cart = () => {
 
         <SheetContent className="w-[80%] sm:w-[30rem] sm:max-w-[30rem]">
           <SheetHeader>
-            <h2 className="font-serif text-center text-3xl mb-6">Корзина</h2>
+            <SheetTitle>
+              <h2 className="font-serif text-center text-3xl mb-6">Корзина</h2>
+            </SheetTitle>
           </SheetHeader>
           {cartIsEmpty ? (
             <p>
@@ -91,60 +92,14 @@ const Cart = () => {
                 }
                 return null
               })}
-            </div>
-          )}
-          {/* {items.length === 0 ? (
-            <p>
-              Ваша корзина пуста. <Link href={'/shop'}>Вернуться к покупкам</Link>
-            </p>
-          ) : (
-            <div className="flex flex-col gap-4">
-              {items.map((item) => (
-                <div key={item.id} className="flex gap-4 bg-white py-2 px-4 rounded-lg">
-                  <div className="flex-shrink-0">
-                    <img
-                      src={typeof item.image === 'string' ? item.image : item.image!.url!}
-                      alt={item.name}
-                      className="w-[100px]"
-                    />
-                  </div>
-                  <div className="flex flex-col gap-1 flex-shrink-0">
-                    <h3 className="text-heading-dark">{item.name}</h3>
-                    <p className="text-sm text-heading opacity-80">Количество: {item.quantity}</p>
-                    <p className="text-sm text-heading opacity-80">
-                      Стоимость:{' '}
-                      <span className="font-bold opacity-100">{item.price * item.quantity}</span>{' '}
-                      руб.
-                    </p>
-                  </div>
-                  <div className="flex justify-between w-full">
-                    <div className="flex items-center h-fit gap-2 self-center">
-                      <button
-                        onClick={() => removeOne(item.id)}
-                        disabled={item.quantity === 1}
-                        className="text-3xl  leading-3 bg-background-light rounded-full p-2 disabled:bg-slate-200 disabled:text-slate-400"
-                      >
-                        <FaMinus size={12} />
-                      </button>
-                      {item.quantity}
-                      <button
-                        onClick={() => addOne(item.id)}
-                        className="text-3xl leading-3 bg-background-light p-2 rounded-full"
-                      >
-                        <FaPlus size={12} />
-                      </button>
-                    </div>
-                    <button onClick={() => removeFromCart(item.id)}>
-                      <FaRegTrashAlt size={20} />
-                    </button>
-                  </div>
-                </div>
-              ))}
               <div className="text-right font-light mt-2">
                 <p className="text-lg ">
                   Итого:{' '}
                   <span className="font-bold text-2xl text-heading-dark">
-                    {items.reduce((acc, item) => acc + item.price * item.quantity, 0)}
+                    {cart?.items?.reduce(
+                      (total, item) => total + (item.product?.price || 0) * item.quantity,
+                      0,
+                    )}
                   </span>{' '}
                   руб.
                 </p>
@@ -152,7 +107,6 @@ const Cart = () => {
                   Доставка: <span className="font-bold">0</span> руб.
                 </p>
               </div>
-
               <button
                 className="rounded-3xl border border-heading
       bg-button hover:bg-button/70 transition-all duration-300
@@ -162,7 +116,7 @@ const Cart = () => {
                 К оплате
               </button>
             </div>
-          )} */}
+          )}
         </SheetContent>
       </Sheet>
     </div>
