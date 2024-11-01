add98cc514b909b9d64879c4838ee1a0badb1170
Dian4ik <jakie16@yandex.ru>
2024-11-01 18:51:25 +0800

    fix suspense

---
+++ src/app/(app)/search/layout.tsx
@@ -0,0 +1,6 @@
+import { ReactNode } from 'react'
+import AppSuspense from '../../../lib/AppSuspense'
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return <AppSuspense>{children}</AppSuspense>
+}
