09bf957d97f330b25e614440d1843038e189ab28
Dian4ik <jakie16@yandex.ru>
2024-11-03 01:36:03 +0800

    afterchange hook

--- src/app/(payload)/collections/Media.ts
+++ src/app/(payload)/collections/Media.ts
@@ -13,4 +13,21 @@ export const Media: CollectionConfig = {
     },
   ],
   upload: true,
+  hooks: {
+    afterChange: [
+      async ({ doc, req }) => {
+        // If the file was just uploaded, ensure it has a URL
+        if (doc.filename && !doc.url) {
+          const generatedUrl = `${process.env.S3_HOST}/products/${doc.filename}`
+
+          // Update the document in the database with the URL
+          await req.payload.update({
+            collection: 'media',
+            id: doc.id,
+            data: { url: generatedUrl },
+          })
+        }
+      },
+    ],
+  },
 }
--- src/app/(payload)/payload.config.ts
+++ src/app/(payload)/payload.config.ts
@@ -64,6 +64,7 @@ export default buildConfig({
         region: process.env.S3_REGION!,
         endpoint: process.env.S3_ENDPOINT!,
       },
+      disableLocalStorage: true,
     }),
   ],
 })
