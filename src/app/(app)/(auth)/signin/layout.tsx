95eedc7351927cd750e6fc7322ee1eb9724e6e5b
Dian4ik <jakie16@yandex.ru>
2024-11-02 21:13:22 +0800

    fixed suspns

--- src/app/(app)/(auth)/signin/layout.tsx
+++ src/app/(app)/(auth)/signin/layout.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from 'react'
-import AppSuspense from '../../../lib/AppSuspense'
+import AppSuspense from '../../../../lib/AppSuspense'
 
 export default function Layout({ children }: { children: ReactNode }) {
   return <AppSuspense>{children}</AppSuspense>
