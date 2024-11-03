d19b512f1c26cd6d5869b495aae7bb6aa4a9bbd4
Dian4ik <jakie16@yandex.ru>
2024-11-03 22:02:50 +0800

    mobile fixes + icons

--- src/app/(app)/search/page.tsx
+++ src/app/(app)/search/page.tsx
@@ -113,7 +113,7 @@ const Page = () => {
       <div className="pt-28 text-center mx-4 sm:mx-16">
         <h1
           className="z-50 text-3xl md:text-4xl lg:text-6xl mb-20
-  w-fit font-serif text-heading-dark relative inline-block"
+  w-fit font-serif text-heading-dark relative inline-block text-center"
         >
           Поиск
           <LineSvg className="-left-4 -right-4 -bottom-10 lg:-bottom-20" strokeColor="#822966" />
@@ -157,7 +157,7 @@ const Page = () => {
       <div className="pt-28 text-center mx-4 sm:mx-16">
         <h1
           className="z-50 text-3xl md:text-4xl lg:text-6xl mb-20
-  w-fit font-serif text-heading-dark relative inline-block"
+  w-fit font-serif text-heading-dark relative inline-block text-center"
         >
           Поиск
           <LineSvg className="-left-4 -right-4 -bottom-10 lg:-bottom-20" strokeColor="#822966" />
@@ -203,7 +203,7 @@ const Page = () => {
     <div className="pt-28 text-center mx-4 sm:mx-16">
       <h1
         className="z-50 text-3xl md:text-4xl lg:text-6xl mb-20
-  w-fit font-serif text-heading-dark relative inline-block"
+  w-fit font-serif text-heading-dark relative inline-block text-center"
       >
         Поиск
         <LineSvg className="-left-4 -right-4 -bottom-10 lg:-bottom-20" strokeColor="#822966" />
