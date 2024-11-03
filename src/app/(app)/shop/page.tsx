2e6b3a5e8637f397179ae7f14a76f437dd9a7539
Dian4ik <jakie16@yandex.ru>
2024-11-03 22:34:22 +0800

    design fixes

--- src/app/(app)/shop/page.tsx
+++ src/app/(app)/shop/page.tsx
@@ -44,7 +44,10 @@ export default function Page() {
       w-fit font-serif text-heading-dark relative inline-block"
       >
         Каталог
-        <LineSvg className="-left-4 -right-4 -bottom-12 lg:-bottom-20" strokeColor="#822966" />
+        <LineSvg
+          className="-left-4 -right-4 -bottom-12 lg:-bottom-20 mx-auto"
+          strokeColor="#292461"
+        />
       </h1>
 
       <div className="w-full flex justify-center gap-6 xs:gap-4 md:gap-6 flex-wrap">
